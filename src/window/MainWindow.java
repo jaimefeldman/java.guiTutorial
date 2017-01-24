bd87a85efd23806788b3e01f90255a116ab5dff8
Jaime Feldman <jaimefel@gmail.com>
2017-01-24 14:56:03 -0400

    se ha creado un area de texto junto con un boton en un border layout al presionar el boton agrega un texto al textAria

--- src/window/MainWindow.java
+++ src/window/MainWindow.java
@@ -1,16 +1,40 @@
 package window;
 
+import java.awt.BorderLayout;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
+	private TextArea textAria;
+	private JButton  clickButton;
+	
 	public MainWindow() {
 		
 		setTitle("Window Tutorial");
 		setSize(600, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		
+		textAria 	= new TextArea();
+		clickButton = new JButton("click me");
+		clickButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				textAria.append("Click!\n");
+			}
+		});
+		
+		add(textAria, BorderLayout.CENTER);
+		add(clickButton,  BorderLayout.SOUTH);
+
 		setLocationRelativeTo(null);
 
 	}
